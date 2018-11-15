a53a89734c1790a1f66e42af599293a34fafc643
amnarciso <amnarciso@gmail.com>
2018-11-15 18:11:07 -0200

    mvp

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ var webdriver = require("selenium-webdriver"),
 	until = webdriver.until;
 
 var chromeCapabilities = webdriver.Capabilities.chrome();
-var chromeOptions = {
-	    'args': ['--disable-infobars', '--window-position=0,0']
-	};
-	chromeCapabilities.set('chromeOptions', chromeOptions);
+var chromeOptions = {'args': ['--disable-infobars']};
+chromeCapabilities.set('chromeOptions', chromeOptions);
 var driver = new webdriver.Builder().withCapabilities(chromeCapabilities).build();
-driver.manage().window().setRect({x: 0, y: 10000, width: 640, height: 480});
+driver.manage().window().setRect({x: 0, y: 0, width: 640, height: 480});
+var browser = new Browser(driver);
 
 //Read providers config
 var fs = require("fs");
@@ -24,62 +23,98 @@ var app = express();
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
  
-app.get('/', function (req, res) {
-	(async function() {
-	  try {
-		await driver.get('http://mastertv.biz/entrar.htm');
-		await driver.findElement(By.xpath('//input[@name="login"]')).sendKeys('amnarciso@gmail.com');
-		await driver.findElement(By.xpath('//input[@name="senha"]')).sendKeys('mypass');
-		await driver.findElement(By.xpath('//input[@value="Continuar"]')).click();
-
-	    await driver.switchTo().frame(driver.findElement(By.xpath('//iframe[@src="master.php"]')));
-	    await driver.findElement(By.xpath('//a[text()="Camais 750"]')).click();
-
-	    await driver.switchTo().frame(driver.findElement(By.xpath('//iframe[@src="frente.php"]')));
-	    await driver.findElement(By.xpath('//a[text()="CANAIS EM SERVIDORES NO BR"]')).click();
-
-
-	    let elements = await driver.findElements(By.xpath('//font[parent::td//a]'));
-		for(let elem of elements) {
-		    let text = await elem.getAttribute("innerText");
-		    console.log(text.trim());
+(async function() {
+	var p_id  = 0;
+	for (let provider of providers){
+		provider.id = p_id;
+		p_id ++;
+		var m_id = 0;
+		for (let menu of provider.menus){
+			menu.id = m_id;
+			m_id ++;
+			for (let order of provider.login){
+				await browser.runAction(order.action, order.target);
+				await console.log(order.target);
+			}
+			for (let order of menu.show){
+				await browser.runAction(order.action, order.target);
+				await console.log(order.target);
+			}
+			let elemList = await browser.listElements(menu.label.target, menu.label.attribute, menu.label.regex);
+			menu.channels = elemList;
 		}
+	}
 
-
-	  } finally {
-	  	//
-	  }
 	})();
 
-    res.render('home');
+app.get('/', function (req, res) {
+    res.render('home', {providers: providers});
 });
 
-app.get('/test', function (req, res) {
-
+app.get('/:p_id/:m_id/:c_id', function (req, res) {
 	(async function() {
-	  try {
-		await driver.get('http://mastertv.biz/entrar.htm');
-		await driver.findElement(By.xpath('//input[@name="login"]')).sendKeys('amnarciso@gmail.com');
-		await driver.findElement(By.xpath('//input[@name="senha"]')).sendKeys('mypass');
-		await driver.findElement(By.xpath('//input[@value="Continuar"]')).click();
-
-	    await driver.switchTo().frame(driver.findElement(By.xpath('//iframe[@src="master.php"]')));
-	    await driver.findElement(By.xpath('//a[text()="Lista"]')).click();
-
-	    await driver.switchTo().frame(driver.findElement(By.xpath('//iframe[@src="frente.php"]')));
-	    await driver.findElement(By.xpath('//a[text()="CANAIS EM SERVIDORES NO BR"]')).click();
-	    await driver.findElement(By.xpath('(//a[img])[8]')).click();
-
-		let handlePromise = await driver.getAllWindowHandles();
-		await driver.switchTo().window(handlePromise[1]);	
-		await driver.findElement(By.xpath('//div[@id="container"]')).click();
-		await driver.findElement(By.xpath('//div[contains(@class, "jw-icon-fullscreen")]')).click();
-	  } finally {
-	  	//
-	  }
+		for (let order of providers[req.params.p_id].login){
+			await browser.runAction(order.action, order.target);
+			await console.log(order.target);
+		}
+		for (let order of providers[req.params.p_id].menus[req.params.m_id].show){
+			await browser.runAction(order.action, order.target);
+			await console.log(order.target);
+		}
+		await browser.runAction(providers[req.params.p_id].menus[req.params.m_id].load.action, providers[req.params.p_id].menus[req.params.m_id].load.target, req.params.c_id);
+		for (let order of providers[req.params.p_id].menus[req.params.m_id].play){
+			await browser.runAction(order.action, order.target);
+			await console.log(order.target);
+		}
 	})();
- 
-    res.render('home');
+
+    res.render('home', {providers: providers});
 });
  
 app.listen(3000);
+
+function Browser(driver) {
+	this.runAction = async function(action, target, index = null){
+		switch (action) {
+			case 'get':
+				await driver.get(target);
+				break;
+			case 'login':
+				await driver.findElement(By.xpath(target)).sendKeys('amnarciso@gmail.com');
+				break;
+			case 'password':
+				await driver.findElement(By.xpath(target)).sendKeys('mypass');
+				break;
+			case 'click':
+				if (index) {
+				    let elements = await driver.findElements(By.xpath(target));
+					await elements[index].click();
+				} else {
+					await driver.findElement(By.xpath(target)).click();
+				}
+				break;
+			case 'switch':
+			    await driver.switchTo().frame(driver.findElement(By.xpath(target)));
+				break;
+			case 'popup':
+				let handlePromise = await driver.getAllWindowHandles();
+				await driver.switchTo().window(handlePromise[1]);	
+				break;
+		} 
+	}
+
+	this.listElements = async function(target, attrib, regex){
+		var elemList = [];
+		var e_id = 0;
+	    let elements = await driver.findElements(By.xpath(target));
+		for(let elem of elements) {
+		    let text = await elem.getAttribute(attrib);
+		    await elemList.push({
+		    		name: text,
+		    		id: e_id
+		    	});
+		    await e_id ++;
+		}
+		return await elemList;
+	}
+}
